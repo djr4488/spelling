85cc4ec30999aeaa8346409c069337071538685b
Danny Rucker <danny@springventuregroup.com>
2014-09-02 12:09:36 -0500

    Added
    * ParentApi skeleton

---
+++ src/main/java/com/djr/spelling/app/parent/model/UserCreateRequest.java
@@ -0,0 +1,7 @@
+package com.djr.spelling.app.parent.model;
+
+/**
+ * Created by danny.rucker on 9/2/14.
+ */
+public class UserCreateRequest {
+}
--- src/main/java/com/djr/spelling/app/parent/restapi/ParentApi.java
+++ src/main/java/com/djr/spelling/app/parent/restapi/ParentApi.java
@@ -1,7 +1,42 @@
 package com.djr.spelling.app.parent.restapi;
 
+import com.djr.spelling.app.parent.model.UserCreateRequest;
+import com.djr.spelling.app.parent.model.UserLoginRequest;
+import com.djr.spelling.app.parent.service.ParentServiceBean;
+import org.slf4j.Logger;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 /**
  * Created by danny.rucker on 9/2/14.
  */
+@ApplicationScoped
+@Path("parent")
 public class ParentApi {
+	@Inject
+	private Logger log;
+	@Inject
+	private ParentServiceBean parentService;
+
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("createUser")
+	public Response createUser(UserCreateRequest request) {
+		return Response.ok().build();
+	}
+
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("login")
+	public Response login(UserLoginRequest request) {
+		return Response.ok().build();
+	}
 }
