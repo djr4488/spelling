bac266262a265e2cdc0864544096a2823c1dc77f
Danny Rucker <danny@springventuregroup.com>
2014-09-02 12:13:51 -0500

    Added
    * added api classes to Application class

--- src/main/java/com/djr/spelling/app/SpellingApplication.java
+++ src/main/java/com/djr/spelling/app/SpellingApplication.java
@@ -1,7 +1,20 @@
 package com.djr.spelling.app;
 
+import com.djr.spelling.app.child.restapi.ChildApi;
+import com.djr.spelling.app.parent.restapi.ParentApi;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by danny.rucker on 9/2/14.
  */
-public class SpellingApplication {
+@ApplicationPath("/api")
+public class SpellingApplication extends Application {
+	@Override
+	public Set<Class<?>> getClasses() {
+		return new HashSet<Class<?>>(Arrays.asList(ParentApi.class, ChildApi.class));
+	}
 }
